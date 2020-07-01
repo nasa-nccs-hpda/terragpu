ce1b4a23ad68063ba645a1bd7733136241f2a7d1
Jordan A Caraballo-vega <jordan.a.caraballo-vega@nasa.gov>
2020-07-01 10:34:35 -0400

    Update spectralCSI.py

--- spectral/spectralCSI.py
+++ spectral/spectralCSI.py
@@ -1,63 +1,37 @@
+"""
+Purpose: Script to detect cloud/shadow for multi/hyperspectral images.
+Adapted from paper: 
+Zhai, H., Zhang, H., Zhang, L., & Li, P. (2018). Cloud/shadow detection based on spectral 
+indices for multi/hyperspectral optical remote sensing imagery. ISPRS Journal of Photogrammetry 
+and Remote Sensing, 144, 235-253.
+Author: Jordan A Caraballo Vega, Science Data Processing Branch, 587.0
+"""
+
+import rasterio
+import xarray as xr
+
+img = rasterio.open('red.tif')
+print (img.is_tiled)
+print (img.block_shapes)
+
+
+
+
+
+
+
+
+"""
 import PIL.Image
 import tensorflow as tf
 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 mpl.rcParams['figure.figsize'] = (12, 5)
-
 import numpy as np
 import tifffile as tiff           # read tiff files
 import rasterio
 import pandas as pd
-
-def visualize(original, augmented):
-  fig = plt.figure()
-  plt.subplot(1,2,1)
-  plt.title('Original image')
-  plt.imshow(original)
-
-  plt.subplot(1,2,2)
-  plt.title('Augmented image')
-  plt.imshow(augmented)
-
-def localStandardization(images, filename='normalization_data', ndata=pd.DataFrame(), strategy='per-batch'):
-    """
-    Applies zero-center standardization using mean and standard deviation for each channel.
-    Channel mean and standard deviation are saved for use during inference.
-    Args:
-        images: numpy array in the format (n, w, h, c).
-        filename: filename to store mean and std data.
-        ndata: pandas dataframe with mean and std values for each channel. Only for predictions.
-        strategy: can select between per-image or per-batch.
-    Returns: locally standardized numpy array.
-    """
-    if not ndata.empty: # for inference only
-        for i in range(images.shape[-1]): # for each channel in images
-            # standardize all images based on given mean and std
-            images[:, :, :, i] = (images[:, :, :, i] - ndata['channel_mean'][i]) / ndata['channel_std'][i]
-        return images
-    elif strategy == 'per-batch': # for all images in batch
-        f = open(filename + "_norm_data.csv", "w+") # open file to store mean and std values
-        f.write("i,channel_mean,channel_std,channel_mean_post,channel_std_post\n") # write column labels
-        for i in range(images.shape[-1]): # for each channel in images
-            channel_mean = np.mean(images[:, :, :, i]) # mean for each channel
-            channel_std  = np.std(images[:, :, :, i]) # std for each channel
-            images[:, :, :, i] = (images[:, :, :, i] - channel_mean)/channel_std # standardize
-            channel_mean_post = np.mean(images[:, :, :, i]) # checking post mean - should be ~0.0
-            channel_std_post = np.std(images[:, :, :, i]) # checking post std - should be ~1.0
-            # write to file for each channel
-            f.write(f'{i},{channel_mean},{channel_std},{channel_mean_post},{channel_std_post}\n')
-        f.close() # close file
-    elif strategy == 'per-image': # standardization for each image individually
-        for i in range(images.shape[0]): # for each image
-            for j in range(images.shape[-1]): # for each channel in images
-                channel_mean = np.mean(images[i, :, :, j]) # mean for each channel
-                channel_std  = np.std(images[i, :, :, j])  # std for each channel
-                images[i, :, :, j] = (images[i, :, :, j] - channel_mean) / channel_std # standardize
-    else:
-        sys.exit(f'Standardization strategy {strategy} not supported. Check list of supported methods.')
-    return images
-
 image = tiff.imread('image2_20190228_data.tif')
 
 ## Identify clouds
@@ -88,7 +62,7 @@ image = tiff.imread('image2_20190228_data.tif')
 
 ## General final segmentation map
 
-"""
+
 image = localStandardization(image, filename='normalization_data', ndata=pd.DataFrame(), strategy='per-batch')
 image = np.squeeze(image, axis=0)
 
