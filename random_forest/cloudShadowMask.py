e8db1b7d7ca5082130359f672e9baae3aed82af9
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2020-07-21 23:28:35 -0400

    Adding contour.

--- random_forest/cloudShadowMask.py
+++ random_forest/cloudShadowMask.py
@@ -13,19 +13,42 @@ raster     = np.array(ds.GetRasterBand(1).ReadAsArray())
 raster[raster < 0] = 0
 
 print (raster.shape, type(raster))
-print (np.unique(raster))
+raster = np.uint8(raster)
+print (np.unique(raster), type(raster))
 
 #gray = cv2.cvtColor(raster, cv2.COLOR_BGR2GRAY)
 gray = raster
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-#thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
+thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
 
 import matplotlib.pyplot as plt 
 import matplotlib.pyplot as plt
 import matplotlib.colors as pltc
+import imutils
 
 cc = pltc.ListedColormap(['black', 'white'])
-colormap = plt.imshow(blurred)
-colormap.set_cmap(cc)
 
 plt.imsave('test_blurred_bw.png', blurred, cmap=cc)
+plt.imsave('test_thresh_bw.png', thresh, cmap=cc)
+
+# find contours in the thresholded image
+cnts = cv2.findContours(blurred.copy(), cv2.RETR_EXTERNAL,
+    cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+print (len(cnts))
+
+# loop over the contours
+for c in cnts[:40]:
+    # compute the center of the contour
+    M = cv2.moments(c)
+    #cX = int(M["m10"] / M["m00"])
+    #cY = int(M["m01"] / M["m00"])
+    # draw the contour and center of the shape on the image
+    cv2.drawContours(raster, [c], -1, (0, 255, 0), 2)
+    #cv2.circle(raster, (cX, cY), 7, (255, 255, 255), -1)
+    #cv2.putText(raster, "center", (cX - 20, cY - 20),
+    #    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    # show the image
+    #cv2.imshow("Image", image)
+    #cv2.waitKey(0)
+cv2.imwrite("Image.png", raster)
