70ed7c9e2787f1e1a961639cbab76368bc11f88b
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2020-09-05 01:07:22 -0400

    Restructuring project.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='xrasterlib',
+    version='0.0.1',
+    description='Methods for remote sensing satellite imagery processing',
+    author='Jordan A. Caraballo-Vega',
+    author_email='jordan.a.caraballo-vega@nasa.gov',
+    zip_safe=False,
+    url='https://github.com/jordancaraballo/xrasterlib.git',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
