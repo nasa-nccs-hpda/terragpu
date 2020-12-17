369b9045cf8b90ca483eb1151fca5ef321047b12
Jordan A Caraballo-Vega <jordan.a.caraballo-vega@nasa.gov>
2020-12-16 22:40:31 -0500

    Added get_model to CNN class.

--- xrasterlib/dl/cnn.py
+++ xrasterlib/dl/cnn.py
@@ -11,6 +11,8 @@ import tensorflow as tf
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras.callbacks import TensorBoard, CSVLogger
 from tensorflow.keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.optimizers import Adam, Adadelta
+from xrasterlib.dl.network.unet import unet_batchnorm, unet_dropout
 
 try:
     import cupy as cp
@@ -62,7 +64,18 @@ class CNN(Raster):
         self.y_train = None
         self.y_test = None
 
+        # Model parameters
+        self.nclass = int(self.cfg_data['nclasses'])
+        self.tilesize = int(self.cfg_data['tilesize'])
+        self.nchannels = int(self.cfg_data['nchannels'])
+        self.train_nchannels = int(self.cfg_train['nchannels'])
+
+        # self.ntiles = int(dfrast['ntiles'].sum())
+        self.input_size = (self.tilesize, self.tilesize, self.train_nchannels)
+        self.maps = list(map(int, self.cfg_train['maps'].split(' ')))
+
         # Train hyperparameters
+        self.lr = float(self.cfg_train['lr'])
         self.bsize = int(self.cfg_train['bsize'])
         self.epochs = int(self.cfg_train['epochs'])
         self.startepoch = int(self.cfg_train['startepoch'])
@@ -70,9 +83,12 @@ class CNN(Raster):
         # Train parameters
         self.modelsave = self.cfg_train['modelsave']
         self.savedir = self.cfg_train['savedir']
+
         self.get_loss()
         self.get_metrics()
         self.get_callbacks()
+        self.get_optimizer()
+        self.get_model()
 
         # store prediction if required
         self.prediction = None
@@ -151,6 +167,32 @@ class CNN(Raster):
             map(callbacks.get, self.cfg_train['callbacks'].split(' '))
         )
 
+    def get_optimizer(self):
+        """
+        Get optimizer function.
+        """
+        opts = {
+            'Adam': Adam(lr=self.lr),
+            'Adadelta': Adadelta(lr=self.lr),
+        }
+        self.optimizer = \
+            opts.get(self.cfg_train['optimizer'], "Invalid optimizer")
+
+    def get_model(self):
+        """
+        Get model function.
+        """
+        networks = {
+            'unet_batchnorm': unet_batchnorm(
+                nclass=self.nclass, input_size=self.input_size, maps=self.maps
+            ),
+            'unet_dropout': unet_dropout(
+                nclass=self.nclass, input_size=self.input_size, maps=self.maps
+            ),
+        }
+        self.model = \
+            networks.get(self.cfg_train['network'], "Invalid network")
+
 
 class GC_Callback(tf.keras.callbacks.Callback):
     """
@@ -178,7 +220,9 @@ if __name__ == "__main__":
     # 1. Create raster object
     if 1 in unit_tests:
         logging.info(f"Unit Test #1: {cnn_obj.loss} {cnn_obj.metrics}")
-        logging.info(f"Unit Test #1: {cnn_obj.callbacks}")
+        logging.info(f"Unit Test #1: {cnn_obj.callbacks} {cnn_obj.optimizer}")
+        logging.info(f"Unit Test #1: {cnn_obj.model}")
+
 
     #    raster = Raster(filename, bands)
     #    assert raster.data.shape[0] == 8, "Number of bands should be 8."
